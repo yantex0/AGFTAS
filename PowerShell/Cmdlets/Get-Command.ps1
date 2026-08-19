16a4067bf746d65f2425991ecbe7b7c75f13be6c
yantex0 <192586328+yantex0@users.noreply.github.com>
2026-08-19 13:38:29 +1000

    Create Get-Command.ps1

---
+++ PowerShell/Cmdlets/Get-Command.ps1
@@ -0,0 +1,6 @@
+# Used to find commands if you know ANYTHING about it.
+
+# You are trying to find info about something? Likely will be a 'Get' command. Use `Get-Command -Verb Get`
+# You are doing something with aliases? Use `Get-Command -Noun Alias`
+
+Get-Command "<search-query>"
